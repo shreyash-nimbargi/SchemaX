6c0d47f9d5059d4c699a6eb20c38686e40dca8f2
shreyash-nimbargi <shreenim27@gmail.com>
2026-06-09 16:03:53 +0530

    Configure backend for hosted MySQL

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,17 @@
+import os
 import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def get_connection():
     connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="nl_to_sql_project"
+        host=os.getenv("MYSQL_HOST"),
+        port=int(os.getenv("MYSQL_PORT")),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        database=os.getenv("MYSQL_DATABASE"),
+        ssl_disabled=False
     )
     return connection
