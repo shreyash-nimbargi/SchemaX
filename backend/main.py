630d342b7a024e795ff692dfec592cd2b863c0b9
shreyash-nimbargi <shreenim27@gmail.com>
2026-06-09 16:44:14 +0530

    Allow deployed frontend origin

--- backend/main.py
+++ backend/main.py
@@ -10,10 +10,10 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "https://schemax.vercel.app"
-],
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://schemax.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
