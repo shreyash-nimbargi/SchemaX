b2eeb3599f9cefa449e645c6776d289895180af4
shreyash-nimbargi <shreenim27@gmail.com>
2026-06-09 16:41:37 +0530

    Use deployed backend URL in frontend

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,11 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://schemax.vercel.app"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
